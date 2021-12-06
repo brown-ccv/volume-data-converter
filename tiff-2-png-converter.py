55668d2b17bd6603cb386b6c25d9032a0e5104da
Kmilo9999 <k.diaz99@gmail.com>
2021-12-06 14:07:41 -0500

    finished rgb channels merged into a single rgb image

--- tiff-2-png-converter.py
+++ tiff-2-png-converter.py
@@ -5,6 +5,10 @@ import cv2
 import numpy as np
 import imagesize
 import math
+import tifffile  as tiff
+import numpngw
+
+verbose = False
 
 def convert_tiff_directoryAction(source_path, dest_path):
     list_files,width, heigth = list_folder_files(source_path)
@@ -13,27 +17,37 @@ def convert_tiff_directoryAction(source_path, dest_path):
     image_g = [None] * num_images
     image_b = [None] * num_images
     images = [None] * num_images
+    bits_per_sample = ""
     for i in range(num_images):
         file_full_path = list_files[i]
-        img = cv2.imread(list_files[i])
+        img = tiff.imread(list_files[i])
+        if bits_per_sample=="":
+            bits_per_sample = img.dtype
+        elif bits_per_sample!=img.dtype:
+            print("image " + file_full_path + "has a different sample type "+bits_per_sample+" expected: "+bits_per_sample)
+            sys.exit("Check images bits per sample tag")
         name, extension = os.path.splitext(file_full_path)
         if "ch1" in name:
-             print("Load Image " + str(i) + " Channel 1 - "+ file_full_path )
+             if verbose:
+                print("Load Image " + str(i) + " Channel 1 - "+ file_full_path )
              image_r[i] = img
         elif "ch2" in name:
-             print("Load Image " + str(i) + " Channel 2 - "+ file_full_path )
+             if verbose:
+                print("Load Image " + str(i) + " Channel 2 - "+ file_full_path )
              image_g[i] = img
         elif "ch3" in name:
-             print("Load Image " + str(i) + " Channel 3 - "+ file_full_path )
+             if verbose:
+                print("Load Image " + str(i) + " Channel 3 - "+ file_full_path )
              image_b[i] = img
         else:
-             print("Load Image " + str(i) + " RGB - "+ file_full_path )
+             if verbose:
+                print("Load Image " + str(i) + " RGB - "+ file_full_path )
              image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB )
              images[i] = image_rgb
              
 
     if len(image_r) != 0 or len(image_g) != 0 or len(image_b) != 0:
-        merge_RGB(image_r, image_g, image_b, images,width,heigth,num_images)
+        merge_RGB(image_r, image_g, image_b, images,width,heigth,num_images,bits_per_sample)
 
     saveToImage(images,num_images,width, heigth,dest_path)
   
@@ -42,8 +56,9 @@ def saveToImage(volume,depth,width,height, dest_path):
     dim = math.ceil(math.sqrt(z_slices))
     max_res = math.floor(4096 / dim)
     downscale = math.ceil( max(width, height) / max_res)
-    print("resolution_image_before " + str(width)+ " "+str( height))  
-    print("downscale " + str(downscale))  
+    if verbose:
+        print("resolution_image_before " + str(width)+ " "+str( height))  
+        print("downscale " + str(downscale))  
 	
     resolution_image = ( int(width / downscale), int(height / downscale))
     
@@ -72,40 +87,42 @@ def saveToImage(volume,depth,width,height, dest_path):
     name, extension = os.path.splitext(dest_path)
     if not extension:
         extension = ".png"
-      
-    status = cv2.imwrite(name+extension,image_out)
+    
+    #normalized_image= cv2.normalize(img,  image_out, 0, 255)
+    #status = cv2.imwrite(name+extension,image_out)
+    numpngw.write_png(name+extension,image_out)
     f = open(name+"_metadata", "a")
     f.write("Width:"+str(width)+"\n")
     f.write("Heigth:"+str(height)+"\n")
     f.write("Depth:"+str(depth)+"\n")
     f.close()
-    print("Image written to file-system : ",status)
+    print("Image written to file-system : "+name+extension)
     
 
     
-def merge_RGB(image_r,  image_g,  image_b, rgb_images, width, heigh, depth):
-    zero_image = np.zeros((width,heigh))
+def merge_RGB(image_r,  image_g,  image_b, rgb_images, width, heigh, depth,bits_per_sample):
+    zero_image = np.zeros((width,heigh),dtype=bits_per_sample)
     for z in range(depth):
-        r_channel = np.zeros((width,heigh))
-        g_channel = np.zeros((width,heigh))
-        b_channel = np.zeros((width,heigh))
+        r_channel = np.zeros((width,heigh),dtype=bits_per_sample)
+        g_channel = np.zeros((width,heigh),dtype=bits_per_sample)
+        b_channel = np.zeros((width,heigh),dtype=bits_per_sample)
         if image_r[z] is not None:
-            tmp = image_r[z]
-            r_channel = tmp[:,:,0]
+            
+            r_channel = image_r[z]
         else:
             r_channel = zero_image
         if image_g[z] is not None:
-            tmp = image_g[z]
-            g_channel = tmp[:,:,1]
+            
+            g_channel = image_g[z]
         else:
             g_channel = zero_image
         if image_b[z] is not None:
-            tmp = image_b[z]
-            b_channel = tmp[:,:,2]
+            
+            b_channel = image_b[z]
         else:
             b_channel = zero_image
-        merged_image = cv2.merge((r_channel,g_channel,b_channel))
-        rgb_images.append(merged_image)
+        merged_image = np.dstack([r_channel,g_channel,b_channel])
+        rgb_images[z]= merged_image
 
 
     
@@ -145,10 +162,14 @@ def main():
    parser.add_argument("-d", "--dest",
                         required=True,
                         help="Path to the directory and filename of the result merged image (png by default) will be saved. ")
+   parser.add_argument("--verbose",
+                        help="new name for files in directory",action='store_true')
                         
    args = parser.parse_args()
    name, extension = os.path.splitext(args.dest)
-   print(name)
+   if args.verbose:
+       verbose = True
+
    if os.path.exists(args.source) and os.path.exists(os.path.dirname(name)):
        convert_tiff_directoryAction(args.source,args.dest)
    else:
